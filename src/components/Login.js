325a34fa47c88aa33792c9b6566699966274ee5d
Evraldi <pekkapower9090@gmail.com>
2024-07-26 10:16:56 +0700

    a

--- src/components/Login.js
+++ src/components/Login.js
@@ -37,7 +37,7 @@ const Login = () => {
     }
 
     try {
-      const response = await axios.post('/auth/login', { email, password });
+      await axios.post('/auth/login', { email, password });
       alert('Login successful');
     } catch (error) {
       console.error(error);
