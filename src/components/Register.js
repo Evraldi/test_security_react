65a0a67ce36ea22d46a1cf871f99d2243abd9356
Evraldi <pekkapower9090@gmail.com>
2024-07-28 15:42:57 +0700

    apalahcuy

--- src/components/Register.js
+++ src/components/Register.js
@@ -4,24 +4,24 @@ import axios from '../axios';
 const Register = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [username, setUsername] = useState('');
+  const [name, setName] = useState('');
   const [emailError, setEmailError] = useState('');
   const [passwordError, setPasswordError] = useState('');
-  const [usernameError, setUsernameError] = useState('');
+  const [nameError, setNameError] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   const validateInputs = () => {
     let isValid = true;
 
-    if (username.trim() === '') {
-      setUsernameError('Username is required');
+    if (name.trim() === '') {
+      setNameError('Username is required');
       isValid = false;
-    } else if (username.length < 5) {
-      setUsernameError('Username must be at least 5 characters long');
+    } else if (name.length < 5) {
+      setNameError('Username must be at least 5 characters long');
       isValid = false;
     } else {
-      setUsernameError('');
+      setNameError('');
     }
 
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -52,11 +52,11 @@ const Register = () => {
 
     setIsLoading(true);
     try {
-      await axios.post('/auth/register', { email, password, username });
+      await axios.post('/auth/register', { email, password, name });
       alert('Registration successful');
       setEmail('');
       setPassword('');
-      setUsername('');
+      setName('');
     } catch (error) {
       console.error(error);
       setErrorMessage('Registration failed: ' + (error.response?.data?.msg || 'Please try again.'));
@@ -81,16 +81,16 @@ const Register = () => {
         {emailError && <p id="email-error" style={{ color: 'red' }}>{emailError}</p>}
       </label>
       <label>
-        Username:
+        name:
         <input 
           type='text' 
-          value={username} 
-          onChange={(e) => setUsername(e.target.value)} 
+          value={name} 
+          onChange={(e) => setName(e.target.value)} 
           aria-required="true"
-          aria-invalid={!!usernameError}
-          aria-describedby="username-error"
+          aria-invalid={!!nameError}
+          aria-describedby="name-error"
         />
-        {usernameError && <p id="username-error" style={{ color: 'red' }}>{usernameError}</p>}
+        {nameError && <p id="name-error" style={{ color: 'red' }}>{nameError}</p>}
       </label>
       <label>
         Password:
